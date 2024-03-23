0f410e5db8d1fb71043056912e71484f66a50a00
Jbrockhoff <150721892+Jbrockhoff@users.noreply.github.com>
2024-03-23 14:16:36 -0500

    delete button working

--- public/js/profile.js
+++ public/js/profile.js
@@ -45,5 +45,23 @@ const newFormHandler = async (event) => {
 // document
 //   .querySelector('.post-list')
 //   .addEventListener('click', delButtonHandler);
+const delButtonHandler = async (event) => {
+  if (event.target.matches('.btn-danger')) {
+    const id = event.target.getAttribute('data-id');
 
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document
+  .querySelector('.post-list')
+  .addEventListener('click', delButtonHandler);
   
